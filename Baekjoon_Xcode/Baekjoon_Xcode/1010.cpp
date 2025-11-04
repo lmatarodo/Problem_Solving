90a035c6d8702b20f35a35d869dadd8cf6c6c11b
동규 <donggyu@dong-gyuui-MacBookPro.local>
2025-11-04 11:08:15 +0900

    xcode 설정 추가

---
+++ Baekjoon_Xcode/Baekjoon_Xcode/1010.cpp
@@ -0,0 +1,7 @@
+//
+//  1010.cpp
+//  Baekjoon_Xcode
+//
+//  Created by 동규 on 11/4/25.
+//
+
---
+++ Baekjoon_Xcode/Baekjoon_Xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Baekjoon_Xcode
+//
+//  Created by 동규 on 11/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
