d23b507a89978ec20fb1cccc666daab5cb72de16
서동규 <lmatarodo@gmail.com>
2026-01-15 20:05:00 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool cmp(int a, int b){
+    return a > b;
+}
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), cmp);
+    
+    for (int i = 0; i < citations.size(); i++) {
+        if ( citations[i] >= i + 1){
+            answer = i + 1; 
+        }
+        
+    }
+    return answer;
+}
