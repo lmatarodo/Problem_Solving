067edfbfe2b86d5dbc34166e6b9118ec69774fa1
서동규 <lmatarodo@gmail.com>
2026-02-09 19:59:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 200 ms, Memory: 6400 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -8,24 +8,14 @@ int n, m;
 vector<int> adj[1002];
 bool vis[1002];
 
-int bfs() {
-    int num = 0;
-    for (int i = 1; i <= n; i++) {
-        if (vis[i]) continue;
-        num++;
-        queue<int> q;
-        q.push(i);
-        vis[i] = true;
-        while (!q.empty()) {
-            int curr = q.front(); q.pop();
-            for (auto nxt: adj[curr]) {
-                if (vis[nxt]) continue;
-                q.push(nxt);
-                vis[nxt] = true;
-            }
-        }
+void dfs(int curr) {
+    
+    vis[curr] = true;
+    for (auto nxt: adj[curr]) {
+        if (vis[nxt]) continue;
+        dfs(nxt);
     }
-    return num;
+    
 }
 
 int main() {
@@ -35,6 +25,13 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    cout << bfs();
+    int num = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            num++;
+            dfs(i); // 연결된 정점이 하나라도 있고 아직 방문 x
+        }
+    }
 
+    cout << num;
 }
