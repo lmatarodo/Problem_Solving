b55411bc2f9e2fcfda1711270cfc92b5e902a86a
서동규 <lmatarodo@gmail.com>
2026-03-12 11:47:10 +0900

    [level 3] Title: 순위, Time: 1.10 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,45 +1,51 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
+vector<vector<int>> winadj(102);
+vector<vector<int>> loseadj(102); // 각각 win / lose 그래프
+bool visited[102];
+
+int bfs(int st, vector<vector<int>>& graph) {
+    queue<int> q;
+    fill(visited, visited + 102, false);
+    
+    q.push(st);
+    visited[st] = true;
+    int cnt = 0;    
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        
+        for (int nxt: graph[cur]) {
+            if(visited[nxt]) continue;
+            visited[nxt] = true;
+            q.push(nxt);
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
+
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
-    const int INF = 10000;
-    vector<int> adj[n + 1];
-    int dist[n + 1][n + 1]; // A에서 B로 가는 최단 경로 저장
     
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++){
-            if (i == j) dist[i][j] = 0;
-            else dist[i][j] = INF;
-        }
-    }
-    
-    for (auto result: results) {
-        adj[result[0]].push_back(result[1]);
-        dist[result[0]][result[1]] = 1;
-    }
-    
-    for (int i = 1; i <= n; i++) { // 경유지
-        for (int s = 1; s <= n; s++) { // 출발지
-            for (int t = 1; t <= n; t++) { // 도착지
-                if (dist[s][t] > dist[s][i] + dist[i][t]) {
-                    dist[s][t] = dist[s][i] + dist[i][t];
-                }
-            }
-        }
+    for (auto& result: results) {
+        winadj[result[0]].push_back(result[1]);
+        loseadj[result[1]].push_back(result[0]);
     }
     
     for (int i = 1; i <= n; i++) {
-        int cnt = 0;
-        for (int j = 1; j <= n; j++) {
-            if (i == j) continue;
-            if (dist[i][j] != INF || dist[j][i] != INF) cnt++;
-        }
-        if (cnt == n - 1) answer++;
+        int wincnt = bfs(i, winadj);
+        int losecnt = bfs(i, loseadj);
+        
+        if (wincnt + losecnt == n - 1)
+            answer++;
+        
     }
-    
     
     return answer;
 }
