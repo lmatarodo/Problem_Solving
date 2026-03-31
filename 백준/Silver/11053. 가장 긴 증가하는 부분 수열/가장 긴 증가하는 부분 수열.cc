b2830b87a89e6987a9021bee62463a2f47c5740d
서동규 <lmatarodo@gmail.com>
2026-03-31 17:43:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -6,22 +6,46 @@ using namespace std;
 
 int n;
 
-int main() {
-    cin >> n;
-    vector<int> v;
-    int num;
+// int main() {
+//     cin >> n;
+//     vector<int> v;
+//     int num;
 
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        if (v.empty()) v.push_back(num);
-        if (!v.empty() && v.back() < num) v.push_back(num);
-        if (!v.empty() && v.back() >= num) {
-            auto it = lower_bound(v.begin(), v.end(), num);
-            *it = num;
+//     for (int i = 0; i < n; i++) {
+//         cin >> num;
+//         if (v.empty()) v.push_back(num);
+//         if (!v.empty() && v.back() < num) v.push_back(num);
+//         if (!v.empty() && v.back() >= num) {
+//             auto it = lower_bound(v.begin(), v.end(), num);
+//             *it = num;
+//         }
+//     }
+
+//     cout << v.size();
+
+//     return 0;
+// }
+
+int dp[1002];
+int arr[1002];
+
+int main() {
+
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+
+    dp[0] = 1;
+    for (int i = 1; i < n; i++) {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
         }
     }
-
-    cout << v.size();
+    int result = *max_element(dp, dp + n);
+    cout << result;
 
     return 0;
 }
