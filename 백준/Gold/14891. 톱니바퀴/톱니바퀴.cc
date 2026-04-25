1506298d0a2b7aa5c07209a876ff4efeee0de7fa
서동규 <lmatarodo@gmail.com>
2026-04-25 17:37:48 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -1,75 +1,74 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
 int k;
 int wheels[4][8];
+int isRotate[4]; // 0: 정지, 1: 시계, -1: 반시계
 
-void rotateUnit(int idx, int dir) {
-    if (dir == 0) return;
-
-    if (dir == 1) {
-        int temp = wheels[idx][7];
-        for (int i = 7; i > 0; i--)
-            wheels[idx][i] = wheels[idx][i - 1];
-        wheels[idx][0] = temp;
+void rotateUnit(int wheelNum, int dir) {
+    if (dir == 1) { // 시계방향으로 회전
+        int temp = wheels[wheelNum][7];
+        for (int i = 7; i > 0; i--) 
+            wheels[wheelNum][i] = wheels[wheelNum][i - 1];
+        wheels[wheelNum][0] = temp;
     }
+
     else if (dir == -1) {
-        int temp = wheels[idx][0];
+        int temp = wheels[wheelNum][0];
         for (int i = 0; i < 7; i++) 
-            wheels[idx][i] = wheels[idx][i + 1];
-        wheels[idx][7] = temp;
+            wheels[wheelNum][i] = wheels[wheelNum][i + 1];
+        wheels[wheelNum][7] = temp;
     }
 }
 
-// 핵심 아이디어: 회전 방향을 미리 저장해두고 실제 상태 변경은 나중에 하자!
-void rotateWheel(int startIdx, int dir) {
+void rotateWheels(int wheelNum, int dir) {
+    fill(isRotate, isRotate + 4, 0);
+    isRotate[wheelNum] = dir;
 
-    int dirs[4] = {0, }; // 각 바퀴의 회전 방향을 저장
-    dirs[startIdx] = dir;
-
-    for (int i = startIdx; i < 3; i++) {
-        if (wheels[i][2] != wheels[i + 1][6]) {
-            dirs[i + 1] = -dirs[i];
-        }
-        else break;
+    // 현재 돌려야 하는 바퀴 인덱스보다 높은 것들의 회전 방향을 정함
+    for (int i = wheelNum; i < 3; i++) {
+        if (wheels[i + 1][6] != wheels[i][2]) 
+            isRotate[i + 1] = -isRotate[i];
+        else break; // 인접한 극성이 같으면 더 이상 전파되지 않음
     }
 
-    for (int i = startIdx; i > 0; i--) {
-        if (wheels[i - 1][2] != wheels[i][6]) {
-            dirs[i - 1] = -dirs[i];
-        }
+    // 현재 돌려야 하는 바퀴 인덱스보다 낮은 것들의 회전 방향을 정함
+    for (int i = wheelNum; i > 0; i--) {
+        if (wheels[i - 1][2] != wheels[i][6]) 
+            isRotate[i - 1] = -isRotate[i];
         else break;
-    }
-
-    for (int i = 0; i < 4; i++) {
-        rotateUnit(i, dirs[i]);
     }
 }
+
 
 int main() {
-    string wheel;
+    string input;
     for (int i = 0; i < 4; i++) {
-        cin >> wheel;
+        cin >> input;
         for (int j = 0; j < 8; j++) {
-            wheels[i][j] = wheel[j] - '0';
+            wheels[i][j] = input[j] - '0';
+        }
+    }
+    
+    cin >> k;
+    int wheelNum, dir;
+    for (int i = 0; i < k; i++) {
+        cin >> wheelNum >> dir;
+        rotateWheels(wheelNum - 1, dir);
+        for (int j = 0; j < 4; j++) {
+            rotateUnit(j, isRotate[j]);
         }
     }
 
-    cin >> k;
-    int startIdx, dir;
-    for (int i = 0; i < k; i++) {
-        cin >> startIdx >> dir;
-        rotateWheel(startIdx - 1, dir);
-    }
-    int ans = 0;
+    int score = 0;
     for (int i = 0; i < 4; i++) {
-        if (wheels[i][0] == 1){
-            ans += pow(2, i);
-        }
+        if (wheels[i][0] == 1) 
+            score += pow(2, i);
     }
-    cout << ans;
+    cout << score;
 
     return 0;
 }
