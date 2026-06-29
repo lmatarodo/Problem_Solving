d1a984c7f746908c5c92257396d32208c2e9940a
서동규 <lmatarodo@gmail.com>
2026-06-29 15:29:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.51 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,39 +1,50 @@
-#include<vector>
+#include <vector>
 #include <queue>
+#include <algorithm>
+
+#define X first
+#define Y second
+
 using namespace std;
 
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int dist[101][101];
+
+int bfs(vector<vector<int>>& maps) {
     int n = maps.size();
     int m = maps[0].size();
-    vector<vector<int>> visit(n, vector<int>(m, -1));
-    queue<pair<int, int>> q;
-    int dx[4] = {1, 0, -1, 0};
-    int dy[4] = {0, 1, 0, -1};
     
+    for (int i = 0; i < n; i++)
+        fill(dist[i], dist[i] + m, -1);
+    
+    queue<pair<int, int>> q;
     q.push({0, 0});
-    visit[0][0] = 0;
+    dist[0][0] = 0;
     
     while (!q.empty()) {
-        auto curr = q.front();
-        q.pop();
+        auto cur = q.front(); q.pop();
         
         for (int dir = 0; dir < 4; dir++) {
-            int nx = curr.first + dx[dir];
-            int ny = curr.second + dy[dir];  
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) 
-                continue;
-            if (visit[nx][ny] >= 0 || maps[nx][ny] == 0) continue;
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
             
-            visit[nx][ny] = visit[curr.first][curr.second] + 1;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (maps[nx][ny] == 0 || dist[nx][ny] != -1) continue;
+            
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             q.push({nx, ny});
         }
     }
     
-    if (visit[n - 1][m - 1] == -1) answer = -1;
-    else answer = visit[n - 1][m - 1] + 1;
-    
-    
+    if (dist[n - 1][m - 1] == -1) return -1;
+    return dist[n - 1][m - 1] + 1;
+}
+
+int solution(vector<vector<int>> maps)
+{
+    int answer = 0;
+    answer = bfs(maps);
     return answer;
 }
