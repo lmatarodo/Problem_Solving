238e0579ec151ad878c26a5a3ff066f8f8a7da5d
서동규 <lmatarodo@gmail.com>
2026-07-04 14:49:48 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -4,26 +4,17 @@
 
 using namespace std;
 
-int n;
-bool visited[10002]; // 티켓 별로 방문했는지 인덱스를 의미함
-vector<string> answer;
-
-// return true를 안하면 경로를 찾아도 return을 통해 돌아오게 됨
-// bool형을 반환하게 하여 
-bool dfs(string cur, int depth, vector<vector<string>>& tickets) {
-    if (depth == n) { // 모든 공항을 돔
-        return true; // 참을 반환하게 해야하고 종료시킴
-    }
+bool dfs(string cur, vector<vector<string>>& tickets, vector<bool>& visited ,vector<string>& answer) {
+    if (answer.size() == tickets.size() + 1) return true;
     
     for (int i = 0; i < tickets.size(); i++) {
         if (!visited[i] && tickets[i][0] == cur) {
             visited[i] = true;
             answer.push_back(tickets[i][1]);
-            
-            if (dfs(tickets[i][1], depth + 1, tickets)) return true; // 재귀 호출 결과가 true면 그대로 종료
-            
-            answer.pop_back();
+            if (dfs(tickets[i][1], tickets, visited, answer))
+                return true;
             visited[i] = false;
+            answer.pop_back();
         }
     }
     
@@ -31,11 +22,13 @@ bool dfs(string cur, int depth, vector<vector<string>>& tickets) {
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    int n = tickets.size();
+    vector<bool> visited(n + 1, false);
     
     sort(tickets.begin(), tickets.end());
-    n = tickets.size();
-    answer.push_back("ICN");
-    dfs("ICN", 0, tickets);
     
+    answer.push_back("ICN");
+    dfs(answer[0], tickets, visited, answer);
     return answer;
 }
