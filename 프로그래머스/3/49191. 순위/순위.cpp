c132ed6f801d104b43c273578ea8ca57fe72b297
서동규 <lmatarodo@gmail.com>
2026-07-10 11:02:29 +0900

    [level 3] Title: 순위, Time: 41.68 ms, Memory: 4.67 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,50 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
 
 using namespace std;
 
-vector<vector<int>> winadj(102);
-vector<vector<int>> loseadj(102); // 각각 win / lose 그래프
-bool visited[102];
-
-int bfs(int st, vector<vector<int>>& graph) {
-    queue<int> q;
-    fill(visited, visited + 102, false);
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    vector<vector<bool>> graph(n + 1, vector<bool>(n + 1, false));
     
-    q.push(st);
-    visited[st] = true;
-    int cnt = 0;    
-    while (!q.empty()) {
-        int cur = q.front(); q.pop();
-        
-        for (int nxt: graph[cur]) {
-            if(visited[nxt]) continue;
-            visited[nxt] = true;
-            q.push(nxt);
-            cnt++;
+    for (auto r: results) {
+        graph[r[0]][r[1]] = true;
+    }
+    
+    for (int k = 1; k <= n; k++) {
+        for (int s = 1; s <= n; s++) {
+            for (int t = 1; t <= n; t++) {
+                if (graph[s][k] && graph[k][t]) graph[s][t] = true;
+            }
         }
     }
     
-    return cnt;
-}
-
-int solution(int n, vector<vector<int>> results) {
-    int answer = 0;
-    
-    for (auto& result: results) {
-        winadj[result[0]].push_back(result[1]);
-        loseadj[result[1]].push_back(result[0]);
-    }
-    
     for (int i = 1; i <= n; i++) {
-        int wincnt = bfs(i, winadj);
-        int losecnt = bfs(i, loseadj);
-        
-        if (wincnt + losecnt == n - 1)
-            answer++;
-        
+        int cnt = 0;
+        for (int j = 1; j <= n; j++) {
+            //if (i == j) continue;
+            if (graph[i][j] || graph[j][i]) cnt++;
+        }
+        if (cnt == n - 1) answer++;
     }
     
     return answer;
