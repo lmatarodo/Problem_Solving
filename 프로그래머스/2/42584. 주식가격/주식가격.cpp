e8f534aea473464d4fa28666a5e926c73f890ec4
서동규 <lmatarodo@gmail.com>
2026-08-02 17:46:30 +0900

    [level 2] Title: 주식가격, Time: 22.44 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,13 +7,15 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     int n = prices.size();
     vector<int> answer(n, 0);
-    stack<int> st; // 가격이 아직 떨어지지 않은 가격들의 인덱스
     
-    for (int i = 0; i < n; i++) {
+    stack<int> st;
+    for (int i = 0; i < prices.size(); i++) {
+        // 가격이 떨어졌으면, 그 시점을 저장
         while (!st.empty() && prices[st.top()] > prices[i]) {
             answer[st.top()] = i - st.top();
             st.pop();
         }
+            
         st.push(i);
     }
     
