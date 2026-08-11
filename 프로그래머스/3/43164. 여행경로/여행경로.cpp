96e04c1c7cb0c994b3bb86fb50f5c4dd64556b72
서동규 <lmatarodo@gmail.com>
2026-08-11 22:11:44 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -4,31 +4,30 @@
 
 using namespace std;
 
-bool dfs(string cur, vector<vector<string>>& tickets, vector<bool>& visited ,vector<string>& answer) {
-    if (answer.size() == tickets.size() + 1) return true;
+bool dfs(string cur, vector<string>& answer, vector<bool>& isused, vector<vector<string>>& tickets) {
+    if (answer.size() == tickets.size() + 1) {
+        return true;
+    }
     
     for (int i = 0; i < tickets.size(); i++) {
-        if (!visited[i] && tickets[i][0] == cur) {
-            visited[i] = true;
+        if (tickets[i][0] == cur && !isused[i]) {
+            isused[i] = true;
             answer.push_back(tickets[i][1]);
-            if (dfs(tickets[i][1], tickets, visited, answer))
-                return true;
-            visited[i] = false;
+            if (dfs(tickets[i][1], answer, isused, tickets)) return true;
+            isused[i] = false;
             answer.pop_back();
         }
     }
-    
     return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
     int n = tickets.size();
-    vector<bool> visited(n + 1, false);
+    vector<bool> isused(n, false);
     
     sort(tickets.begin(), tickets.end());
-    
     answer.push_back("ICN");
-    dfs(answer[0], tickets, visited, answer);
+    dfs("ICN", answer, isused, tickets);
     return answer;
 }
