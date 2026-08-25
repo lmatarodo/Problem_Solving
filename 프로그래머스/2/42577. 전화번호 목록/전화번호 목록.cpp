80ab1158fcea9e9615501aecc30365cd6956e695
서동규 <lmatarodo@gmail.com>
2026-08-25 14:51:05 +0900

    [level 2] Title: 전화번호 목록, Time: 44.13 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -9,11 +9,13 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     
     sort(phone_book.begin(), phone_book.end());
+    
     for (int i = 0; i < phone_book.size() - 1; i++) {
         if (phone_book[i + 1].find(phone_book[i]) == 0) {
             answer = false;
-            break;
+            return answer;
         }
     }
+    
     return answer;
 }
