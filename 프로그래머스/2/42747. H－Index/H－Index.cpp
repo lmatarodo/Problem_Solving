4b40a18810cb3e874a652eaeaacad9b86a953dd9
서동규 <lmatarodo@gmail.com>
2026-08-25 16:32:24 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-    
     sort(citations.begin(), citations.end(), greater<int>());
-    for (int i = 0; i <= citations.size(); i++) { // h-index 후보는 0부터 n-1
-        int cnt = 0;
-        for (int j = 0; j < citations.size(); j++) {
-            if (citations[j] >= i) cnt++; // h-index 이상 인용되었다면 카운트 올림
+    
+    for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] >= i + 1) {
+            answer = i + 1;
         }
-        if (i <= cnt) // 만약 최종 카운트가 h-index 이상이면, 정답 후보
-            answer = max(answer, i);
     }
-     
+    
     return answer;
 }
