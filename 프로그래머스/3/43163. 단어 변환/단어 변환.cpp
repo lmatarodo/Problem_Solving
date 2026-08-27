1908ec0af6357a076347109f0024acf451cdef0b
서동규 <lmatarodo@gmail.com>
2026-08-27 16:44:14 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-bool isDiffone(string a, string b) {
+bool isDiffOne(string a, string b) {
     int cnt = 0;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) cnt++;
-        if (cnt > 2) return false; // 조기 종료
+        if (cnt >= 2) return false;
     }
     
     if (cnt == 1) return true;
@@ -18,26 +18,27 @@ bool isDiffone(string a, string b) {
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
     int n = words.size();
+    vector<bool> isvisited(n, false);
     
-    vector<int> dist(n + 1, -1);
-    queue<pair<string, int>> q; // 두번째 int는 words에 있는 인덱스 번호
-    q.push({begin, -1});
+    queue<pair<string, int>> q;
+    q.push({begin, 0});
     
     while (!q.empty()) {
         auto cur = q.front(); q.pop();
+        string curname = cur.first; int curdist = cur.second;
         
-        if (cur.first == target) answer = dist[cur.second] + 1;
+        if (curname == target) {
+            answer = cur.second;
+            return answer;
+        }
         
         for (int i = 0; i < words.size(); i++) {
-            if (dist[i] == -1 && isDiffone(cur.first, words[i])) {
-                q.push({words[i], i});
-                if (cur.second == -1) dist[i]++;
-                else dist[i] = dist[cur.second] + 1;
+            if (!isvisited[i] && isDiffOne(words[i], curname)) {
+                isvisited[i] = true;
+                q.push({words[i], curdist + 1});
             }
-            
         }
     }
-
     
     return answer;
 }
