2543fd3f85a656ca9782a133c8e33a4e88bfb75e
서동규 <lmatarodo@gmail.com>
2026-08-28 11:13:29 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
-bool dfs(string cur, vector<string>& answer, vector<bool>& isused, vector<vector<string>>& tickets) {
+bool dfs(string cur_city, vector<bool>& isused ,vector<string>& answer, vector<vector<string>>& tickets) {
+        
     if (answer.size() == tickets.size() + 1) {
         return true;
     }
     
     for (int i = 0; i < tickets.size(); i++) {
-        if (tickets[i][0] == cur && !isused[i]) {
+        if (!isused[i] && tickets[i][0] == cur_city) {
             isused[i] = true;
             answer.push_back(tickets[i][1]);
-            if (dfs(tickets[i][1], answer, isused, tickets)) return true;
+            if (dfs(tickets[i][1], isused, answer, tickets))
+                return true;
             isused[i] = false;
             answer.pop_back();
         }
@@ -28,6 +30,7 @@ vector<string> solution(vector<vector<string>> tickets) {
     
     sort(tickets.begin(), tickets.end());
     answer.push_back("ICN");
-    dfs("ICN", answer, isused, tickets);
+    dfs("ICN", isused, answer, tickets);
+    
     return answer;
 }
