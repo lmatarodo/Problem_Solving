cf3903936bac4f53605f95eca414b7f5a2d85c81
서동규 <lmatarodo@gmail.com>
2026-08-30 17:55:54 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,9 +1,31 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(string name) {
     int answer = 0;
+    
+    for (int i = 0; i < name.size(); i++) {
+        answer += min('Z' - name[i] + 1, name[i] - 'A');
+    }
+    
+    int n = name.size();
+    int move = n - 1;
+    for (int i = 0; i < n; i++) {
+        int next = i + 1;
+        
+        while (next < n && name[next] == 'A') {
+            next++;
+        }
+        
+        int right = 2 * i + (n - next);
+        int left = 2 * (n - next) + i;
+        
+        move = min({move, right, left});
+    }
+    answer += move;
+    
     return answer;
 }
