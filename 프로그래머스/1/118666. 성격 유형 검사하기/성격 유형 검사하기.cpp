40382897273ca6d1e0af714cb415705b0f6c2633
서동규 <lmatarodo@gmail.com>
2026-09-05 11:14:53 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.03 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> survey, vector<int> choices) {
+    string answer = "";
+    
+    map<char, int> m; // 성격 유형, 점수
+    for (int i = 0; i < survey.size(); i++) {
+        if (choices[i] < 4) {
+            m[survey[i][0]] += 4 - choices[i];
+        }
+        
+        else {
+            m[survey[i][1]] += choices[i] - 4;
+        }
+    }
+    
+    if (m['R'] >= m['T']) answer += "R";
+    else  answer += "T";
+    
+    if (m['C'] >= m['F']) answer += "C";
+    else answer += "F";
+    
+    if (m['J'] >= m['M']) answer += "J";
+    else answer += "M";
+    
+    if (m['A'] >= m['N']) answer += "A";
+    else answer += "N";
+    
+    return answer;
+}
